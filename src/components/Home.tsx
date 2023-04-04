590e63dcc607ef37e801f877ca466fa91d4e55f6
emileCda <emile.cda@gmail.com>
2023-04-04 09:14:11 +0200

    responsive row/column

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,38 +1,37 @@
-
 /**
  * this function do ...
  */
 
-import { DivW50, FlexCol, Section1 } from "../styles/Home.style";
+import { DivImage, DivText,  Section1 } from "../styles/Home.style";
 
 // import { Header, HomeMainContainer, MainArticle, PicActicle, TextArticle } from "../styles/Home.style";
-
 
 export default function Home() {
   return (
     <>
       <main>
+        <Section1>
+          <DivImage>
+            <img src="src/images/making-street-food-nha-trang-new.jpg" alt="" />
+          </DivImage>
 
-{/* <!-- SECTION 1 --> */}
+          <DivText>
+            <h1>nem Ninh Hoa</h1>
+            <h1>Nem nuong ou le nem grillé</h1>
+            <p>
+              Essayons ensuite le Nem nuong ou le nem grillé à base de porc
+              fermenté grillé. En dégustant «nem Ninh Hoa», vous sentirez
+              l’arôme de la viande, de l’acide sucré, du croquant et du mélange
+              épicé, c’est très intéressant! Le nem nuong est servi notamment
+              avec une salade de papaye verte qui apporte une nouvelle saveur.{" "}
+            </p>
 
-<Section1 >
-
-    <DivW50 >
-        <img  src="src/images/making-street-food-nha-trang-new.jpg" alt="" />
-    </DivW50>
-
-    <DivW50 ><FlexCol>
-        <h1> nem Ninh Hoa</h1>
-        <h2> Nem nuong ou le nem grillé</h2>
-        <p>Essayons ensuite le Nem nuong ou le nem grillé à base de porc fermenté grillé. En dégustant «nem Ninh Hoa», vous sentirez l’arôme de la viande, de l’acide sucré, du croquant et du mélange épicé, c’est très intéressant! Le nem nuong est servi notamment avec une salade de papaye verte qui apporte une nouvelle saveur.  </p>
-
-        <a href="#" className="p-1 bckg-red color-white">En savoir plus</a>
-        </FlexCol></DivW50>
-
-</Section1>
-
-
-</main>
+            <a href="#" className="p-1 bckg-red color-white">
+              En savoir plus
+            </a>
+          </DivText>
+        </Section1>
+      </main>
     </>
   );
 }
--- src/styles/Home.style.tsx
+++ src/styles/Home.style.tsx
@@ -5,118 +5,38 @@ import { device } from "../lib/Common";
 export const Section1 = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  /* min-width: 100vw; */
-  /* align-content : center; */
-  background-color: ${AppTheme.colors.background};
-  border 1px dashed blue;
-  @media ${device.tablet} { 
-    /* background-color: ${AppTheme.colors.lessBG}; */
+  @media ${device.tablet} {
+    display: flex;
     flex-direction: row;
   }
-
 `;
-export const DivW50 = styled.div`
-  width: 50vw;
-  padding: 0 1.5rem;
-  @media ${device.tablet} { 
-    /* background-color: ${AppTheme.colors.lessBG}; */
-    padding: 2.5rem;
-    width: 100vw;
-  }
 
-border 1px solid red;
-`;
-export const ZDivW50 = styled.div`
-  width: 100%;
+export const DivImage = styled.div`
   display: flex;
-  justify-content: center;
-  padding: 0 1.2rem;
-  border 1px solid red;
+  max-width: 100vw;
+  justify-content : center;
+
+  @media ${device.tablet} {
+    max-width: 50vw;
+  }
   img {
-    -webkit-box-shadow: 0px 0px 5px 0px rgba(255, 255, 255, 0.3);
-    box-shadow: 0px 0px 5px 0px rgba(255, 255, 255, 0.3);
-    min-width: 100%;
-    border-radius : .6rem;
-    display: block;
-    width:385px;
-  /* max-height:100%; */
-  /* width: auto; */
-  height: 300px;
-
-  }
-  h1, h2 {
-    min-width: 100%;
-    padding: 0 1.2rem;
-    background-color: ${AppTheme.colors.background};
-    color: ${AppTheme.colors.lessFG};
-  }
-  p {
-    min-width: 100%;
-    padding: 0 1.2rem;
-    color: ${AppTheme.colors.forground};
-  }
-  a {
-    padding: 0 1rem;
-    background-color: ${AppTheme.colors.background};
-    color: ${AppTheme.colors.lessFG};
-  }
-
-`;
-
-export const FlexCol = styled.div`
-  flex-direction: column;
-`;
-
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%ù
-export const HomeMainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  min-width: 100vw;
-  min-height: 100vh;
-`;
-
-export const Header = styled.div`
-  background-color: ${AppTheme.colors.forground};
-  min-width: 100vw;
-  text-align: center;
-`;
-
-export const MainArticle = styled.div`
-  background-color: ${AppTheme.colors.forground};
-  display: flex;
-  flex-direction: column;
-  padding: ${AppTheme.BorderRadius};
-  min-width: 100vw;
-  border: 1px solid red;
-`;
-
-export const TextArticle = styled.div`
-  background-color: ${AppTheme.colors.forground};
-  display: flex;
-  flex-wrap: auto;
-  min-width: 100vw;
-  padding: ${AppTheme.BorderRadius};
-  p {
-    padding: 0;
-    margin: 0;
-    color: ${AppTheme.colors.lessFG};
+    max-width: 90%;
+    border-radius: 0.6rem;
   }
 `;
 
-export const PicActicle = styled.div`
-  background-color: ${AppTheme.colors.forground};
-  display: flex;
-  justify-content: center;
+export const DivText = styled.div`
+  max-width: 100vw;
+  padding: 1rem;
 
-  img {
-    max-width: 100vw;
-    padding: ${AppTheme.BorderRadius};
-    /* max-height: 230px; */
-    width: auto;
-    height: auto;
-    display: bloc;
-    border-radius: 1rem;
+  h1 {
+    font-size: 1.5rem;
+    text-align: center;
+  }
+
+  @media ${device.tablet} {
+    max-width: 50vw;
+    display: flex;
+    flex-direction: column;
   }
 `;
